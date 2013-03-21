1153f1b7c67a5113c7330ad570fe3c6e16d64d1e
ddepablo <ddepablo001@gmail.com>
2013-03-21 19:13:12 +0100

    Registro, superGrupo, Grupo, Solista

--- src/packGroups/Registro.java
+++ src/packGroups/Registro.java
@@ -1,5 +1,27 @@
 package packGroups;
 
-public class Registro {
+import java.util.ArrayList;
 
+public class Registro {
+	private ArrayList<SuperGrupo> listaRegistro;
+	
+	/*
+	 * instancia unica de la clase
+	 */
+	private static Registro mRegistro = new Registro();
+
+	/*
+	 * Constructor privado de la clase
+	 */
+	private Registro() {
+	}
+
+	/*
+	 * Metodo que devuelve la instacia unica
+	 */
+	public static Registro getRegistro() {
+		return mRegistro;
+	}
+	
+	
 }
