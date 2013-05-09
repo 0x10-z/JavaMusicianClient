2bcfd5b29acc38c6627055dc3697021096bc87c7
ddepablo <ddepablo001@gmail.com>
2013-05-09 17:41:01 +0200

    .

--- src/Interfaz/VentanaPrincipal.java
+++ src/Interfaz/VentanaPrincipal.java
@@ -24,7 +24,7 @@ public class VentanaPrincipal extends JDialog {
 	VentanaArtistas va = new VentanaArtistas();
 
 	ManejoFicheros mn = new ManejoFicheros();
-	
+
 	/**
 	 * Launch the application.
 	 */
