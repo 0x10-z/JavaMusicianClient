55b50f0276c67eac09bdfefc442fbb64db3f48e5
ander arambalza <anderarambalza@gmail.com>
2013-05-21 12:08:12 +0200

    wefdwedew

--- backup.sql
+++ backup.sql
@@ -23,11 +23,14 @@ DROP TABLE IF EXISTS `artista`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `artista` (
-  `nombre` varchar(15) NOT NULL,
-  `alias` varchar(15) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `alias` varchar(30) NOT NULL,
   `edad` int(11) NOT NULL,
-  `disponible` char(2) NOT NULL,
-  PRIMARY KEY (`alias`)
+  `instrumento` varchar(30) NOT NULL,
+  `disponibilidad` char(2) NOT NULL,
+  PRIMARY KEY (`alias`),
+  KEY `instrumento` (`instrumento`),
+  CONSTRAINT `artista_ibfk_1` FOREIGN KEY (`instrumento`) REFERENCES `instrumento` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -37,8 +40,107 @@ CREATE TABLE `artista` (
 
 LOCK TABLES `artista` WRITE;
 /*!40000 ALTER TABLE `artista` DISABLE KEYS */;
-INSERT INTO `artista` VALUES ('ander','cover',25,'no');
+INSERT INTO `artista` VALUES ('ander','cover',25,'teclado','si'),('borja','coyi',25,'triangulo','no'),('diego','ddepablo',20,'bateria','si'),('iker','ocio',20,'guitarra','no');
 /*!40000 ALTER TABLE `artista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `biografia`
+--
+
+DROP TABLE IF EXISTS `biografia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `biografia` (
+  `nombre` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL,
+  `hitos` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`nombre`,`id`),
+  CONSTRAINT `fkbio` FOREIGN KEY (`nombre`) REFERENCES `artista` (`alias`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `biografia`
+--
+
+LOCK TABLES `biografia` WRITE;
+/*!40000 ALTER TABLE `biografia` DISABLE KEYS */;
+INSERT INTO `biografia` VALUES ('cover',1,'2001-->tema jarto'),('cover',2,'2002-->tema muuy jarto');
+/*!40000 ALTER TABLE `biografia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `nombre` varchar(30) NOT NULL,
+  `nom_artista` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`nombre`,`nom_artista`),
+  KEY `nom_artista` (`nom_artista`),
+  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`nom_artista`) REFERENCES `artista` (`alias`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo`
+--
+
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+INSERT INTO `grupo` VALUES ('equipo_B','cover'),('equipo_B','coyi'),('equipo_B','ddepablo'),('equipo_B','ocio');
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `instrumento`
+--
+
+DROP TABLE IF EXISTS `instrumento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `instrumento` (
+  `nombre` varchar(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `instrumento`
+--
+
+LOCK TABLES `instrumento` WRITE;
+/*!40000 ALTER TABLE `instrumento` DISABLE KEYS */;
+INSERT INTO `instrumento` VALUES ('acordeon'),('bajo'),('bateria'),('flauta'),('guitarra'),('guitarra_electrica'),('piano'),('saxofon'),('tambor'),('teclado'),('triangulo'),('trompeta'),('viola'),('violin');
+/*!40000 ALTER TABLE `instrumento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solista`
+--
+
+DROP TABLE IF EXISTS `solista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solista` (
+  `nombre` varchar(30) NOT NULL,
+  PRIMARY KEY (`nombre`),
+  CONSTRAINT `fknom` FOREIGN KEY (`nombre`) REFERENCES `artista` (`alias`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solista`
+--
+
+LOCK TABLES `solista` WRITE;
+/*!40000 ALTER TABLE `solista` DISABLE KEYS */;
+INSERT INTO `solista` VALUES ('cover');
+/*!40000 ALTER TABLE `solista` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +152,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-05-16  8:40:29
+-- Dump completed on 2013-05-19 13:04:01
